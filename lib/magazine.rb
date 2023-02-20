ba8005aab6194dbb43460bf399b64f2b22694e0c
m-njora <mmuthekijora@gmail.com>
2023-02-20 15:28:23 +0300

    added folders

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -1,14 +1,26 @@
+require 'pry'
 class Magazine
     #The name and category of the magazine can be changed after being initialized.
 
     attr_accessor :name, :category
+
+    @@all = []
+
 
         #A magazine is initialized with a name as a string and a category as a string
 
     def initialize(name = "Vogue Magazine" , category = "British Vougue")
         @name = name
         @category = category
+        @@all << self
+
     end
+    def self.all
+        @@all
+
+    end
+    binding.pry
+
 end
 
 mag1 = Magazine.new
